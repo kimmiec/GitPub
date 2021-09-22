d2f261cc1f606b4dd74b060b4f13c9f370ea1d82
kimmiec <89497794+kimmiec@users.noreply.github.com>
2021-09-22 19:09:07 -0400

    set up drinks display

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+// require database
+const drinks = require('./models/drink.js');
+
+
 app.get('/', (req, res)=>{
     res.send('Welcome to the Gitpub App!');
 });
 
+// display drinks as json
+app.get('/drinks', (req, res) =>{
+    res.send(drinks);
+});
 
 app.listen(port, ()=>{
     console.log('hi, listening on ' + port);
